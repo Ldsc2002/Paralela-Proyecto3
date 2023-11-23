78291db01a68f5f68fb54108afde095e40addf05
Ldsc2002 <luissantoscuellar@gmail.com>
2023-11-22 20:19:44 -0600

    Code cleanup

--- houghConst.cu
+++ houghConst.cu
@@ -13,158 +13,213 @@ const int degreeBins = 180 / degreeInc;
 const int rBins = 100;
 const float radInc = degreeInc * M_PI / 180;
 
-// CPU implementation of Hough Transform
-void CPU_HoughTran(unsigned char *pic, int w, int h, int **acc) {
-  float rMax = sqrt(1.0 * w * w + 1.0 * h * h) / 2;  
-  *acc = new int[rBins * degreeBins];            
-  memset(*acc, 0, sizeof(int) * rBins * degreeBins); 
-  int xCent = w / 2;
-  int yCent = h / 2;
-  float rScale = 2 * rMax / rBins;
-
-  for (int i = 0; i < w; i++) 
-    for (int j = 0; j < h; j++) 
-      {
-        int idx = j * w + i;
-        if (pic[idx] > 0) 
-          {
-            int xCoord = i - xCent;
-            int yCoord = yCent - j;
-            float theta = 0;         
-            for (int tIdx = 0; tIdx < degreeBins; tIdx++) 
-              {
-                float r = xCoord * cos(theta) + yCoord * sin(theta);
-                int rIdx = (r + rMax) / rScale;
-                (*acc)[rIdx * degreeBins + tIdx]++;
-                theta += radInc;
-              }
-          }
-      }
-}
-
 // Constant memory for CUDA kernel
 __constant__ float d_Cos[degreeBins];
 __constant__ float d_Sin[degreeBins];
 
-// CUDA kernel for Hough Transform with constant memory optimization
-__global__ void GPU_HoughTranConst(unsigned char *pic, int w, int h, int *acc, float rMax, float rScale) {
-  int gloID = blockIdx.x * blockDim.x + threadIdx.x;
-  if (gloID >= w * h) return;
+/* 
+Function: CPU_HoughTran
+Purpose: CPU implementation of Hough Transform
+Arguments:
+    pic - input image
+    w - width of input image
+    h - height of input image
+    acc - output accumulator
+Returns:
+    None
+ */
+void CPU_HoughTran(unsigned char *pic, int w, int h, int **acc) {
+    // Calculate the maximum possible radius
+    float rMax = sqrt(1.0 * w * w + 1.0 * h * h) / 2;  
+    
+    // Initialize the accumulator
+    *acc = new int[rBins * degreeBins];            
+    memset(*acc, 0, sizeof(int) * rBins * degreeBins); 
+    
+    // Calculate the center of the image
+    int xCent = w / 2;
+    int yCent = h / 2;
+    float rScale = 2 * rMax / rBins;
 
-  int xCent = w / 2;
-  int yCent = h / 2;
-  int xCoord = gloID % w - xCent;
-  int yCoord = yCent - gloID / w;
+    // For each pixel in the image
+    for (int i = 0; i < w; i++) 
+        for (int j = 0; j < h; j++) {
+            int idx = j * w + i;
+            
+            if (pic[idx] > 0) {
+                // Calculate the radius for each degree
+                int xCoord = i - xCent;
+                int yCoord = yCent - j;
+                float theta = 0;       
 
-  if (pic[gloID] > 0) {
-    for (int tIdx = 0; tIdx < degreeBins; tIdx++) {
-      float r = xCoord * d_Cos[tIdx] + yCoord * d_Sin[tIdx];
-      int rIdx = (r + rMax) / rScale;
-      atomicAdd(acc + (rIdx * degreeBins + tIdx), 1);
-    }
-  }
+                // For each degree  
+                for (int tIdx = 0; tIdx < degreeBins; tIdx++) {
+                    // Calculate the radius
+                    float r = xCoord * cos(theta) + yCoord * sin(theta);
+                    int rIdx = (r + rMax) / rScale;
+
+                    // Increment the accumulator
+                    (*acc)[rIdx * degreeBins + tIdx]++;
+                    theta += radInc;
+                }
+            }
+        }
 }
 
-// Main function to run Hough Transform
-int main (int argc, char **argv)
-{
-  int i;
-  std::string arg = argv[2];
-  std::size_t pos;
-  int threshold = std::stoi(arg,&pos);
+/* 
+Function: GPU_HoughTranConst
+Purpose: GPU implementation of Hough Transform using constant memory
+Arguments:
+    pic - input image
+    w - width of input image
+    h - height of input image
+    acc - output accumulator
+    rMax - maximum possible radius
+    rScale - scaling factor for radius
+Returns:
+    None
+ */
+__global__ void GPU_HoughTranConst(unsigned char *pic, int w, int h, int *acc, float rMax, float rScale) {
+    int gloID = blockIdx.x * blockDim.x + threadIdx.x;
+    if (gloID >= w * h) return; // out of bounds
 
-  PGMImage* inImg = new PGMImage(argv[1], 1);
+    // Calculate the center of the image
+    int xCent = w / 2;
+    int yCent = h / 2;
+    int xCoord = gloID % w - xCent;
+    int yCoord = yCent - gloID / w;
 
-  int *cpuht;
-  int w = inImg->getXDim();
-  int h = inImg->getYDim();
+    if (pic[gloID] > 0) {
+        // For each degree
+        for (int tIdx = 0; tIdx < degreeBins; tIdx++) {
+            float r = xCoord * d_Cos[tIdx] + yCoord * d_Sin[tIdx];
+            int rIdx = (r + rMax) / rScale;
 
-  cudaEvent_t start, stop;
-  cudaEventCreate(&start);
-  cudaEventCreate(&stop);
-
-  cudaMalloc ((void **) &d_Cos, sizeof (float) * degreeBins);
-  cudaMalloc ((void **) &d_Sin, sizeof (float) * degreeBins);
-
-  // CPU calculation
-  CPU_HoughTran(inImg->getPixels(), w, h, &cpuht);
-
-  // pre-compute values to be stored
-  float *pcCos = (float *) malloc (sizeof (float) * degreeBins);
-  float *pcSin = (float *) malloc (sizeof (float) * degreeBins);
-  float rad = 0;
-  for (i = 0; i < degreeBins; i++)
-  {
-    pcCos[i] = cos (rad);
-    pcSin[i] = sin (rad);
-    rad += radInc;
-  }
-
-  float rMax = sqrt (1.0 * w * w + 1.0 * h * h) / 2;
-  float rScale = 2 * rMax / rBins;
-
-  // TODO eventualmente volver memoria global
-  cudaMemcpyToSymbol(d_Cos, pcCos, sizeof (float) * degreeBins);
-  cudaMemcpyToSymbol(d_Sin, pcSin, sizeof (float) * degreeBins);
-
-  // setup and copy data from host to device
-  unsigned char *d_in, *h_in;
-  int *d_hough, *h_hough;
-
-  h_in = inImg->getPixels(); // h_in contiene los pixeles de la imagen
-
-  h_hough = (int *) malloc (degreeBins * rBins * sizeof (int));
-
-  cudaMalloc ((void **) &d_in, sizeof (unsigned char) * w * h);
-  cudaMalloc ((void **) &d_hough, sizeof (int) * degreeBins * rBins);
-  cudaMemcpy (d_in, h_in, sizeof (unsigned char) * w * h, cudaMemcpyHostToDevice);
-  cudaMemset (d_hough, 0, sizeof (int) * degreeBins * rBins);
-
-  // execution configuration uses a 1-D grid of 1-D blocks, each made of 256 threads
-  //1 thread por pixel
-  int blockNum = ceil (w * h / 256);
-  cudaEventRecord(start);
-  GPU_HoughTranConst <<< blockNum, 256 >>> (d_in, w, h, d_hough, rMax, rScale);
-
-  // get results from device
-  cudaMemcpy (h_hough, d_hough, sizeof (int) * degreeBins * rBins, cudaMemcpyDeviceToHost);
-  cudaEventRecord(stop);
-  
-  // compare CPU and GPU results
-  for (i = 0; i < degreeBins * rBins; i++)
-  {
-    if (cpuht[i] != h_hough[i]) {
-      printf ("Calculation mismatch at : %i %i %i\n", i, cpuht[i], h_hough[i]);
+            // Increment the accumulator
+            atomicAdd(acc + (rIdx * degreeBins + tIdx), 1);
+        }
     }
-  }
-  printf("Done!\n");
-  std::vector<std::pair<int, int>> lines;
-  for (i = 0; i < degreeBins * rBins; i++){
-    if (h_hough[i] > threshold) {
-      // pair order: r, th
-      int my_r = i / degreeBins;
-      int my_th = i % degreeBins;
-      std::pair<int, int> line = {my_r, my_th};
-      lines.push_back(line);
+}
+
+/* 
+Function: main
+Purpose: Driver function for Hough Transform
+Arguments:
+    argc - number of command line arguments
+    argv - command line arguments
+Returns:
+    None
+ */
+int main (int argc, char **argv) {
+    // Initilize variables
+    int i;
+    std::string arg = argv[2];
+    std::size_t pos;
+    int threshold = std::stoi(arg,&pos);
+
+    // Read input image
+    PGMImage* inImg = new PGMImage(argv[1], 1);
+
+    int *cpuht;
+
+    // Get image dimensions
+    int w = inImg->getXDim();
+    int h = inImg->getYDim();
+
+    cudaEvent_t start, stop;
+
+    // Create CUDA events for timing purposes
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
+    // Allocate memory for constant memory
+    cudaMalloc ((void **) &d_Cos, sizeof (float) * degreeBins);
+    cudaMalloc ((void **) &d_Sin, sizeof (float) * degreeBins);
+
+    // CPU calculation
+    CPU_HoughTran(inImg->getPixels(), w, h, &cpuht);
+
+    // Allocate memory for constant memory
+    float *pcCos = (float *) malloc (sizeof (float) * degreeBins);
+    float *pcSin = (float *) malloc (sizeof (float) * degreeBins);
+    float rad = 0;
+
+    // Calculate the cosine and sine values for each degree
+    for (i = 0; i < degreeBins; i++) {
+        pcCos[i] = cos (rad);
+        pcSin[i] = sin (rad);
+        rad += radInc;
     }
-  }
-  inImg->write("ConstOutput.jpeg", lines, radInc, rBins);
 
-  cudaEventSynchronize(stop);
-  float milliseconds = 0;
-  cudaEventElapsedTime(&milliseconds, start, stop);
-  printf("Milliseconds: %.3f ms\n" ,milliseconds);
-  printf("Seconds: %d.%.3d s\n", (int)milliseconds/1000, (int)milliseconds%1000);
+    // Calculate the maximum possible radius
+    float rMax = sqrt (1.0 * w * w + 1.0 * h * h) / 2;
+    float rScale = 2 * rMax / rBins;
 
-  cudaFree((void *) d_Cos);
-  cudaFree((void *) d_Sin);
-  //add
-  cudaFree((void *) d_in);
-	cudaFree((void *) d_hough);
-  delete[] pcCos;
-  delete[] pcSin;
-  delete inImg;
-  cudaDeviceReset();
+    // Copy constant memory to device
+    cudaMemcpyToSymbol(d_Cos, pcCos, sizeof (float) * degreeBins);
+    cudaMemcpyToSymbol(d_Sin, pcSin, sizeof (float) * degreeBins);
+
+    // Setup and copy data from host to device
+    unsigned char *d_in, *h_in;
+    int *d_hough, *h_hough;
+
+    // Get image data
+    h_in = inImg->getPixels();
+
+    h_hough = (int *) malloc (degreeBins * rBins * sizeof (int));
+
+    // Allocate memory on device
+    cudaMalloc ((void **) &d_in, sizeof (unsigned char) * w * h);
+    cudaMalloc ((void **) &d_hough, sizeof (int) * degreeBins * rBins);
+    cudaMemcpy (d_in, h_in, sizeof (unsigned char) * w * h, cudaMemcpyHostToDevice);
+    cudaMemset (d_hough, 0, sizeof (int) * degreeBins * rBins);
+
+    // Launch kernel
+    int blockNum = ceil (w * h / 256);
+    cudaEventRecord(start);
+    GPU_HoughTranConst <<< blockNum, 256 >>> (d_in, w, h, d_hough, rMax, rScale);
+
+    // Get results from device
+    cudaMemcpy (h_hough, d_hough, sizeof (int) * degreeBins * rBins, cudaMemcpyDeviceToHost);
+    cudaEventRecord(stop);
+    
+    // Check for errors
+    for (i = 0; i < degreeBins * rBins; i++) {
+        if (cpuht[i] != h_hough[i]) {
+            printf ("Calculation mismatch at : %i %i %i\n", i, cpuht[i], h_hough[i]);
+        }
+    }
+
+    printf("Done!\n");
+    std::vector<std::pair<int, int>> lines;
+    for (i = 0; i < degreeBins * rBins; i++){
+        if (h_hough[i] > threshold) {
+            int my_r = i / degreeBins;
+            int my_th = i % degreeBins;
+            std::pair<int, int> line = {my_r, my_th};
+            lines.push_back(line);
+        }
+    }
+
+    // Write output image
+    inImg->write("ConstOutput.jpeg", lines, radInc, rBins);
+
+    // Print timing results
+    cudaEventSynchronize(stop);
+    float milliseconds = 0;
+    cudaEventElapsedTime(&milliseconds, start, stop);
+    printf("Milliseconds: %.3f ms\n" ,milliseconds);
+    printf("Seconds: %d.%.3d s\n", (int)milliseconds/1000, (int)milliseconds%1000);
+
+    // Free memory
+    cudaFree((void *) d_Cos);
+    cudaFree((void *) d_Sin);
+    cudaFree((void *) d_in);
+    cudaFree((void *) d_hough);
+    delete[] pcCos;
+    delete[] pcSin;
+    delete inImg;
+    cudaDeviceReset();
 
   return 0;
 }
--- houghShared.cu
+++ houghShared.cu
@@ -13,183 +13,268 @@ const int degreeBins = 180 / degreeInc;
 const int rBins = 100;
 const float radInc = degreeInc * M_PI / 180;
 
-// CPU implementation of Hough Transform
+/* 
+Function: CPU_HoughTran
+Purpose: CPU implementation of Hough Transform
+Parameters:
+    pic: pointer to input image
+    w: width of input image
+    h: height of input image
+    acc: pointer to accumulator array
+Returns: 
+    None
+ */
 void CPU_HoughTran(unsigned char *pic, int w, int h, int **acc) {
-  float rMax = sqrt(w * w + h * h) / 2;
-  *acc = new int[rBins * degreeBins];
-  memset(*acc, 0, sizeof(int) * rBins * degreeBins);
-  int xCent = w / 2;
-  int yCent = h / 2;
-  float rScale = 2 * rMax / rBins;
+    // Calculate the maximum possible radius
+    float rMax = sqrt(w * w + h * h) / 2;
 
-  for (int i = 0; i < w; i++) 
-    for (int j = 0; j < h; j++) {
-      int idx = j * w + i;
-      if (pic[idx] > 0) {
-        int xCoord = i - xCent;
-        int yCoord = yCent - j;
-        float theta = 0;
-        for (int tIdx = 0; tIdx < degreeBins; tIdx++) {
-          float r = xCoord * cos(theta) + yCoord * sin(theta);
-          int rIdx = (r + rMax) / rScale;
-          (*acc)[rIdx * degreeBins + tIdx]++;
-          theta += radInc;
+    // Initialize accumulator array
+    *acc = new int[rBins * degreeBins];
+    memset(*acc, 0, sizeof(int) * rBins * degreeBins);
+    
+    // Calculate the center of the image
+    int xCent = w / 2;
+    int yCent = h / 2;
+    float rScale = 2 * rMax / rBins;
+
+    // Perform Hough Transform
+    for (int i = 0; i < w; i++) {
+        for (int j = 0; j < h; j++) {
+            int idx = j * w + i;
+            
+            if (pic[idx] > 0) {
+                // Calculate the radius for each degree
+                int xCoord = i - xCent;
+                int yCoord = yCent - j;
+                float theta = 0;
+
+                for (int tIdx = 0; tIdx < degreeBins; tIdx++) {
+                    // Calculate the radius
+                    float r = xCoord * cos(theta) + yCoord * sin(theta);
+                    int rIdx = (r + rMax) / rScale;
+
+                    // Increment the accumulator
+                    (*acc)[rIdx * degreeBins + tIdx]++;
+                    theta += radInc;
+                }
+            }
         }
-      }
     }
 }
 
-// GPU Hough Transform kernel
+/* 
+Function: GPU_HoughTran
+Purpose: GPU implementation of Hough Transform
+Parameters:
+    pic: pointer to input image
+    w: width of input image
+    h: height of input image
+    acc: pointer to accumulator array
+    rMax: maximum possible radius
+    rScale: scaling factor for radius
+    d_Cos: pointer to pre-computed cosine values
+    d_Sin: pointer to pre-computed sine values
+Returns:
+    None
+ */
 __global__ void GPU_HoughTran(unsigned char *pic, int w, int h, int *acc, float rMax, float rScale, float* d_Cos, float* d_Sin) {
-  int gloID = blockIdx.x * blockDim.x + threadIdx.x;
-  if (gloID >= w * h) return;
+    // Calculate global ID
+    int gloID = blockIdx.x * blockDim.x + threadIdx.x;
+    if (gloID >= w * h) return; // Check if global ID is valid
 
-  int xCent = w / 2;
-  int yCent = h / 2;
-  int xCoord = gloID % w - xCent;
-  int yCoord = yCent - gloID / w;
+    // Calculate the center of the image
+    int xCent = w / 2;
+    int yCent = h / 2;
+    int xCoord = gloID % w - xCent;
+    int yCoord = yCent - gloID / w;
 
-  if (pic[gloID] > 0) {
-    for (int tIdx = 0; tIdx < degreeBins; tIdx++) {
-      float r = xCoord * d_Cos[tIdx] + yCoord * d_Sin[tIdx];
-      int rIdx = (r + rMax) / rScale;
-      atomicAdd(acc + (rIdx * degreeBins + tIdx), 1);
+    // Perform Hough Transform
+    if (pic[gloID] > 0) {
+        for (int tIdx = 0; tIdx < degreeBins; tIdx++) {
+            // Calculate the radius
+            float r = xCoord * d_Cos[tIdx] + yCoord * d_Sin[tIdx];
+            int rIdx = (r + rMax) / rScale;
+
+            // Increment the accumulator
+            atomicAdd(acc + (rIdx * degreeBins + tIdx), 1);
+        }
     }
-  }
 }
 
-// GPU Hough Transform kernel with shared memory optimization
+/* 
+Function: GPU_HoughTranshared
+Purpose: GPU implementation of Hough Transform with shared memory
+Parameters:
+    pic: pointer to input image
+    w: width of input image
+    h: height of input image
+    acc: pointer to accumulator array
+    rMax: maximum possible radius
+    rScale: scaling factor for radius
+    d_Cos: pointer to pre-computed cosine values
+    d_Sin: pointer to pre-computed sine values
+Returns:
+    None
+ */
 __global__ void GPU_HoughTranshared(unsigned char *pic, int w, int h, int *acc, float rMax, float rScale, float* d_Cos, float* d_Sin) {
-  int gloID = blockIdx.x * blockDim.x + threadIdx.x;
-  if (gloID >= w * h) return;
+    // Calculate global ID
+    int gloID = blockIdx.x * blockDim.x + threadIdx.x;
+    if (gloID >= w * h) return;
 
-  int xCent = w / 2;
-  int yCent = h / 2;
-  int xCoord = gloID % w - xCent;
-  int yCoord = yCent - gloID / w;
+    // Calculate the center of the image
+    int xCent = w / 2;
+    int yCent = h / 2;
+    int xCoord = gloID % w - xCent;
+    int yCoord = yCent - gloID / w;
 
-  __shared__ int localAcc[degreeBins * rBins];
-  for (int i = threadIdx.x; i < degreeBins * rBins; i += blockDim.x)
-    localAcc[i] = 0;
-  __syncthreads();
+    // Initialize local accumulator
+    __shared__ int localAcc[degreeBins * rBins];
+    for (int i = threadIdx.x; i < degreeBins * rBins; i += blockDim.x)
+        localAcc[i] = 0;
+    
+    // Wait for all threads to finish
+    __syncthreads();
 
-  if (pic[gloID] > 0) {
-    for (int tIdx = 0; tIdx < degreeBins; tIdx++) {
-      float r = xCoord * d_Cos[tIdx] + yCoord * d_Sin[tIdx];
-      int rIdx = (r + rMax) / rScale;
-      atomicAdd(&localAcc[rIdx * degreeBins + tIdx], 1);
+    // Perform Hough Transform
+    if (pic[gloID] > 0) {
+        for (int tIdx = 0; tIdx < degreeBins; tIdx++) {
+            // Calculate the radius
+            float r = xCoord * d_Cos[tIdx] + yCoord * d_Sin[tIdx];
+            int rIdx = (r + rMax) / rScale;
+
+            // Increment the accumulator
+            atomicAdd(&localAcc[rIdx * degreeBins + tIdx], 1);
+        }
     }
-  }
 
-  __syncthreads();
+    // Wait for all threads to finish
+    __syncthreads();
 
-  for (int i = threadIdx.x; i < degreeBins * rBins; i += blockDim.x)
-    atomicAdd(&acc[i], localAcc[i]);
+    // Add local accumulator to global accumulator
+    for (int i = threadIdx.x; i < degreeBins * rBins; i += blockDim.x)
+        atomicAdd(&acc[i], localAcc[i]);
 }
 
-// Main function
-int main (int argc, char **argv)
-{
-  int i;
-  std::string arg = argv[2];
-  std::size_t pos;
-  int threshold = std::stoi(arg,&pos);
+/* 
+Function: main
+Purpose: Entry point of the program
+Parameters:
+    argc: number of command line arguments
+    argv: array of command line arguments
+Returns:
+    0 on success, 1 on failure
+ */
+int main (int argc, char **argv) {
+    int i;
+    std::string arg = argv[2];
+    std::size_t pos;
+    int threshold = std::stoi(arg,&pos);
 
-  PGMImage* inImg = new PGMImage(argv[1], 2);
+    // Read input image
+    PGMImage* inImg = new PGMImage(argv[1], 2);
 
-  int *cpuht;
-  int w = inImg->getXDim();
-  int h = inImg->getYDim();
+    int *cpuht;
 
-  float* d_Cos;
-  float* d_Sin;
+    // Get image dimensions
+    int w = inImg->getXDim();
+    int h = inImg->getYDim();
 
-  cudaEvent_t start, stop;
-  cudaEventCreate(&start);
-  cudaEventCreate(&stop);
+    float* d_Cos;
+    float* d_Sin;
 
-  cudaMalloc ((void **) &d_Cos, sizeof (float) * degreeBins);
-  cudaMalloc ((void **) &d_Sin, sizeof (float) * degreeBins);
+    // Create CUDA events for timing purposes
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
 
-  // CPU calculation
-  CPU_HoughTran(inImg->getPixels(), w, h, &cpuht);
+    // Allocate memory for pre-computed values
+    cudaMalloc ((void **) &d_Cos, sizeof (float) * degreeBins);
+    cudaMalloc ((void **) &d_Sin, sizeof (float) * degreeBins);
 
-  // pre-compute values to be stored
-  float *pcCos = (float *) malloc (sizeof (float) * degreeBins);
-  float *pcSin = (float *) malloc (sizeof (float) * degreeBins);
-  float rad = 0;
-  for (i = 0; i < degreeBins; i++)
-  {
-    pcCos[i] = cos (rad);
-    pcSin[i] = sin (rad);
-    rad += radInc;
-  }
+    // CPU calculation
+    CPU_HoughTran(inImg->getPixels(), w, h, &cpuht);
 
-  float rMax = sqrt (1.0 * w * w + 1.0 * h * h) / 2;
-  float rScale = 2 * rMax / rBins;
+    // Pre-compute values to be stored
+    float *pcCos = (float *) malloc (sizeof (float) * degreeBins);
+    float *pcSin = (float *) malloc (sizeof (float) * degreeBins);
+    float rad = 0;
 
-  // TODO eventualmente volver memoria global
-  cudaMemcpy(d_Cos, pcCos, sizeof (float) * degreeBins, cudaMemcpyHostToDevice);
-  cudaMemcpy(d_Sin, pcSin, sizeof (float) * degreeBins, cudaMemcpyHostToDevice);
-
-  // setup and copy data from host to device
-  unsigned char *d_in, *h_in;
-  int *d_hough, *h_hough;
-
-  h_in = inImg->getPixels(); // h_in contiene los pixeles de la imagen
-
-  h_hough = (int *) malloc (degreeBins * rBins * sizeof (int));
-
-  cudaMalloc ((void **) &d_in, sizeof (unsigned char) * w * h);
-  cudaMalloc ((void **) &d_hough, sizeof (int) * degreeBins * rBins);
-  cudaMemcpy (d_in, h_in, sizeof (unsigned char) * w * h, cudaMemcpyHostToDevice);
-  cudaMemset (d_hough, 0, sizeof (int) * degreeBins * rBins);
-
-  // execution configuration uses a 1-D grid of 1-D blocks, each made of 256 threads
-  //1 thread por pixel
-  int blockNum = ceil (w * h / 256);
-  cudaEventRecord(start);
-  GPU_HoughTranshared <<< blockNum, 256 >>> (d_in, w, h, d_hough, rMax, rScale, d_Cos, d_Sin);
-
-  // get results from device
-  cudaMemcpy (h_hough, d_hough, sizeof (int) * degreeBins * rBins, cudaMemcpyDeviceToHost);
-  cudaEventRecord(stop);
-  
-  // compare CPU and GPU results
-  for (i = 0; i < degreeBins * rBins; i++)
-  {
-    if (cpuht[i] != h_hough[i]) {
-      printf ("Calculation mismatch at : %i %i %i\n", i, cpuht[i], h_hough[i]);
+    // Calculate cosine and sine values
+    for (i = 0; i < degreeBins; i++) {
+        pcCos[i] = cos (rad);
+        pcSin[i] = sin (rad);
+        rad += radInc;
     }
-  }
-  printf("Done!\n");
-  std::vector<std::pair<int, int>> lines;
-  for (i = 0; i < degreeBins * rBins; i++){
-    if (h_hough[i] > threshold) {
-      // pair order: r, th
-      int my_r = i / degreeBins;
-      int my_th = i % degreeBins;
-      std::pair<int, int> line = {my_r, my_th};
-      lines.push_back(line);
+
+    // Calculate maximum possible radius and scaling factor
+    float rMax = sqrt (1.0 * w * w + 1.0 * h * h) / 2;
+    float rScale = 2 * rMax / rBins;
+
+    // Allocate memory
+    cudaMemcpy(d_Cos, pcCos, sizeof (float) * degreeBins, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_Sin, pcSin, sizeof (float) * degreeBins, cudaMemcpyHostToDevice);
+
+    // GPU calculation
+    unsigned char *d_in, *h_in;
+    int *d_hough, *h_hough;
+
+    // Get input image pixels
+    h_in = inImg->getPixels();
+    h_hough = (int *) malloc (degreeBins * rBins * sizeof (int));
+    
+    // Allocate memory on device
+    cudaMalloc ((void **) &d_in, sizeof (unsigned char) * w * h);
+    cudaMalloc ((void **) &d_hough, sizeof (int) * degreeBins * rBins);
+    cudaMemcpy (d_in, h_in, sizeof (unsigned char) * w * h, cudaMemcpyHostToDevice);
+    cudaMemset (d_hough, 0, sizeof (int) * degreeBins * rBins);
+
+    // Perform Hough Transform
+    int blockNum = ceil (w * h / 256);
+    cudaEventRecord(start);
+    GPU_HoughTranshared <<< blockNum, 256 >>> (d_in, w, h, d_hough, rMax, rScale, d_Cos, d_Sin);
+
+    // Get results
+    cudaMemcpy (h_hough, d_hough, sizeof (int) * degreeBins * rBins, cudaMemcpyDeviceToHost);
+    cudaEventRecord(stop);
+    
+    // Compare CPU and GPU results
+    for (i = 0; i < degreeBins * rBins; i++) {
+        if (cpuht[i] != h_hough[i]) {
+            printf ("Calculation mismatch at : %i %i %i\n", i, cpuht[i], h_hough[i]);
+        }
     }
-  }
-  inImg->write("SharedOutput.jpeg", lines, radInc, rBins);
+    
+    printf("Done!\n");
+    
+    // Save output image
+    std::vector<std::pair<int, int>> lines;
+    for (i = 0; i < degreeBins * rBins; i++){
+        if (h_hough[i] > threshold) {
+            int my_r = i / degreeBins;
+            int my_th = i % degreeBins;
+            std::pair<int, int> line = {my_r, my_th};
+            lines.push_back(line);
+        }
+    }
+    inImg->write("SharedOutput.jpeg", lines, radInc, rBins);
 
-  cudaEventSynchronize(stop);
-  float milliseconds = 0;
-  cudaEventElapsedTime(&milliseconds, start, stop);
-  printf("Milliseconds: %.3f ms\n" ,milliseconds);
-  printf("Seconds: %d.%.3d s\n", (int)milliseconds/1000, (int)milliseconds%1000);
+    // Print timing results
+    cudaEventSynchronize(stop);
+    float milliseconds = 0;
+    cudaEventElapsedTime(&milliseconds, start, stop);
+    printf("Milliseconds: %.3f ms\n" ,milliseconds);
+    printf("Seconds: %d.%.3d s\n", (int)milliseconds/1000, (int)milliseconds%1000);
 
-  cudaFree((void *) d_Cos);
-  cudaFree((void *) d_Sin);
-  //add
-  cudaFree((void *) d_in);
-	cudaFree((void *) d_hough);
-  delete[] pcCos;
-  delete[] pcSin;
-  delete inImg;
-  cudaDeviceReset();
+    // Free memory
+    cudaFree((void *) d_Cos);
+    cudaFree((void *) d_Sin);
+    cudaFree((void *) d_in);
+    cudaFree((void *) d_hough);
 
-  return 0;
+    delete[] pcCos;
+    delete[] pcSin;
+    delete inImg;
+    cudaDeviceReset();
+
+    return 0;
 }
